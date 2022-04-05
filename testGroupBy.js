e097c6147723df96a967e4f43a2491d51f1d692b
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-04-05 17:39:07 +0530

    Added tests for frequencyCount

--- testGroupBy.js
+++ testGroupBy.js
@@ -3,8 +3,9 @@ const generalTestFuctions = require(testFnPath + '/generalTestFunctions.js');
 const testReport = require(testFnPath + '/testReport.js');
 const groupByLib = require('./groupBy.js');
 
-const groupBy = groupByLib.groupBy;
 const areArraysEqual = groupByLib.areArraysEqual;
+const groupBy = groupByLib.groupBy;
+const frequencyCount = groupByLib.frequencyCount;
 
 // general test functions
 const pushResult = generalTestFuctions.pushResult;
@@ -28,15 +29,40 @@ const groupByTests = function () {
     ['Numbers and strings', [['1'], [2], [1], ['a', 'a'], ['b']], ['1', 2, 1, 'a', 'b', 'a']],
     ['Empty arrays and numbers', [[[], []], [2], [1]], [[], 2, 1, []]],
     ['Only same arrays', [[[1, 2], [1, 2]]], [[1, 2], [1, 2]]],
-    ['Nested Arrays', [[[[1], [2]], [[1], [2]]]], [[[1], [2]], [[1], [2]]]]
+    ['Nested element arrays', [[[[1], [2]], [[1], [2]]]], [[[1], [2]], [[1], [2]]]]
   ];
   for (let index = 0; index < tests.length; index++) {
     testGroupBy(tests[index][0], tests[index][1], tests[index][2]);
   }
 };
 
+const testFrequencyCount = function (description, expected, set) {
+  const actual = frequencyCount(set);
+  const isPassed = areArraysEqual(expected, actual);
+
+  pushResult(isPassed, 'frequencyCount', description, expected, actual);
+  return isPassed;
+};
+
+const frequencyCountTests = function () {
+  const tests = [
+    ['Empty set', [], []],
+    ['Set of numbers', [[1, 2], [2, 1]], [1, 2, 1]],
+    ['Same Arrays and numbers', [[[1, 1], 2], [1, 2]], [[1, 1], 1, [1, 1], 1]],
+    ['Same arrays and string', [[[1, 2], 2], ['1,2', 2]], [[1, 2], '1,2', '1,2', [1, 2]]],
+    ['Numbers and strings', [['1', 1], [2, 1], [1, 1], ['a', 2], ['b', 1]], ['1', 2, 1, 'a', 'b', 'a']],
+    ['Empty arrays and numbers', [[[], 2], [2, 1], [1, 1]], [[], 2, 1, []]],
+    ['Only same arrays', [[[1, 2], 2]], [[1, 2], [1, 2]]],
+    ['Nested element arrays', [[[[1], [2]], 2]], [[[1], [2]], [[1], [2]]]]
+  ];
+  for (let index = 0; index < tests.length; index++) {
+    testFrequencyCount(tests[index][0], tests[index][1], tests[index][2]);
+  }
+};
+
 const allTests = function () {
-  groupByTests();
+  // groupByTests();
+  frequencyCountTests();
   const report = testReport.generateTestReport(getTests());
   testReport.displayReport(report);
 }
